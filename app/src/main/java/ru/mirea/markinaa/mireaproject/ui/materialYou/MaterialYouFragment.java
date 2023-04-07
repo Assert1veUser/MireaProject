63b455b580de3cbd66439f0dc68e74b29fddcbb2
User <tony.markin2017@yande.ru>
2023-04-07 22:10:10 +0300

    init

--- app/src/main/java/ru/mirea/markinaa/mireaproject/ui/materialYou/MaterialYouFragment.java
+++ app/src/main/java/ru/mirea/markinaa/mireaproject/ui/materialYou/MaterialYouFragment.java
@@ -7,38 +7,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 import ru.mirea.markinaa.mireaproject.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MaterialYouFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MaterialYouFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
+    private TextView textView_heading;
+    private TextView textView_main;
+
     public MaterialYouFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment materialYou.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MaterialYouFragment newInstance(String param1, String param2) {
         MaterialYouFragment fragment = new MaterialYouFragment();
         Bundle args = new Bundle();
@@ -60,7 +47,39 @@ public class MaterialYouFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_material_you, container, false);
+        View view = inflater.inflate(R.layout.fragment_material_you, container, false);
+        textView_heading = (TextView) view.findViewById(R.id.textView2);
+        textView_main = (TextView) view.findViewById(R.id.textView3);
+        textView_heading.setText("Интересные факты о создании компьютерных игр");
+        textView_main.setText("Создание компьютерных игр - это захватывающий процесс, который " +
+                "требует множества труда и усилий. В этом блог-посте я расскажу несколько " +
+                "интересных фактов о создании компьютерных игр, которые, возможно, вы еще не знали."
+                + "\n" + "\n" +
+                "1. Название игры может измениться в процессе разработки\n" +
+                "Иногда название игры не подходит к ее концепту. В таком случае разработчики " +
+                "могут изменить название в ходе разработки. Например, изначальное название " +
+                "Grand Theft Auto было \"Race’n’Chase\".\n" +
+                "2. Некоторые игры были созданы по ошибке\n" +
+                "Некоторые знаменитые игры были созданы случайно. Например, Pac-Man был " +
+                "создан как концепт для игры в формате ресторана, но его игровая механика " +
+                "была настолько увлекательной, что его сделали в отдельную игру.\n" +
+                "3. Музыка игр может вдохновляться реальными инструментами\n" +
+                "Многие знаменитые игры включают музыку, которая была написана на реальных " +
+                "музыкальных инструментах. Например, музыка из игры The Elder Scrolls V: Skyrim " +
+                "была написана на органе, а музыка из игры Halo была написана на гитаре.\n" +
+                "4. Некоторые игры могут быть созданы за несколько месяцев\n" +
+                "Несмотря на то, что большинство игр занимают годы разработки, некоторые " +
+                "игры могут быть созданы за несколько месяцев. Например, игра Flappy Bird " +
+                "была создана за два дня.\n" +
+                "5. Игры могут быть созданы на различных платформах\n" +
+                "Существует множество платформ для создания компьютерных игр, включая PC, " +
+                "консоли и мобильные устройства. Некоторые игры могут быть созданы на " +
+                "нескольких платформах одновременно.\n" +
+                "\n" +
+                "Эти факты о создании компьютерных игр подчеркивают трудности и " +
+                "увлекательность процесса и помогут вам лучше понять, что происходит " +
+                "за кулисами. Игры - это не просто развлечение, это целый мир, который " +
+                "создается из того, что есть вокруг нас.");
+        return view;
     }
 }
