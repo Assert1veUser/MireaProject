c377befe3cbacb1c848a860b760652712fedb0d4
User <tony.markin2017@yande.ru>
2023-04-07 10:04:48 +0300

    init

--- app/src/main/java/ru/mirea/markinaa/mireaproject/ui/brouser/BrouserFragment.java
+++ app/src/main/java/ru/mirea/markinaa/mireaproject/ui/brouser/BrouserFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import ru.mirea.markinaa.mireaproject.R;
 
@@ -17,28 +20,17 @@ import ru.mirea.markinaa.mireaproject.R;
  */
 public class BrouserFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public BrouserFragment() {
-        // Required empty public constructor
-    }
+    private WebView webView;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BrouserFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    public BrouserFragment() {
+
+    }
     public static BrouserFragment newInstance(String param1, String param2) {
         BrouserFragment fragment = new BrouserFragment();
         Bundle args = new Bundle();
@@ -55,12 +47,21 @@ public class BrouserFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_brouser, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_brouser, container, false);
+        webView = (WebView) view.findViewById(R.id.webView);
+        webView.loadUrl("https://ya.ru/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        return view;
     }
 }
